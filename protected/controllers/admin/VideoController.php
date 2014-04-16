e3bb9f664d3fbc40d88d6148eefc8f2a7450544a
anhcuongone <anhcuongone@gmail.com>
2014-04-16 16:59:42 +0700

    ok cty

--- protected/controllers/admin/VideoController.php
+++ protected/controllers/admin/VideoController.php
@@ -12,7 +12,45 @@ class VideoController extends Controller
 	public function actiongetAll()
 	{
 		$criteria= new CDbCriteria();
+		$criteria->order= "video_id DESC";
 		$model=Video::model()->findAll($criteria);
 		echo CJSON::encode($model);
+	}
+
+	public function actionsearch()
+	{
+		$video_title='';
+		$category_id='';
+		if (isset($_GET["look"])) {
+           	$video_title=$_GET["look"];
+			$category_id=$_GET["look"];
+        }
+		
+		$query=Yii::app()->db->createCommand()
+					->select("*")
+					->from("video")
+					->where(array("like","video_title","%".$video_title."%"))
+					->orwhere("video_category_id= :category_id",array(":category_id"=>$category_id))
+					->order("video_id DESC")
+					->queryAll()
+					;
+		echo CJSON::encode($query);
+	}
+
+	public function actiondeleteAll()
+	{
+		$recoder=0;
+        $count=1;
+		$data=$_POST['id'];
+		foreach ($data as $item) {
+			$count++;
+			$model=Video::model()->findByPk($item);
+			$recoder=$model->delete();
+		}
+		if ($recoder==1) {
+			echo "Delete ".$count." record successfuly";
+        }else{
+             echo "Delete ".$count." record not successfuly";
+        }
 	}
 }
--- protected/views/admin/video/index.php
+++ protected/views/admin/video/index.php
@@ -19,6 +19,12 @@
 
 		var dataAdapter= new $.jqx.dataAdapter(source);
 
+		$("#btn-search").click(function () {
+                var look=$("#txt-search").val();
+                source.url = "<?php echo Yii::app()->request->baseUrl; ?>/admin/video/search?look="+look;
+                dataAdapter.dataBind();
+        });
+
 		$("#jqxgrid").jqxGrid(
         {
 			width: 1035,
@@ -28,15 +34,18 @@
 	        theme:'ui-redmond',
 	        pagesizeoptions: ['10', '20', '30', '40', '50'],
 	        selectionmode: 'checkbox',
-	        rowsheight: 60,
+	        rowsheight: 90,
+	        autorowheight: true,
+	        columnsresize: true,
 	        columns:[
 	        	{text: "ID", datafield: "video_id", width: 20},
 	        	{text: "Title", datafield: "video_title", width: 150},
-	        	{text: "Description",datafield: "video_description", width: 250},
+	        	{text: "Description",datafield: "video_description", width: 230},
+	        	{text: "Url",datafield: "video_url", width: 230},
 	        	{text: "Create Date", datafield: "video_date_create", width: 150, cellsformat: 'dd-MM-yyyy h:mm:ss tt'},
 	        	{text: "Image", datafield: "video_image", width: 100, height: 200, cellsrenderer: function(row){
 	        		var dataRecord = $("#jqxgrid").jqxGrid('getrowdata', row); 
-	        		return '<img style="margin-left: 5px;" height="60" width="50" src="<?php echo Yii::app()->request->baseUrl; ?>/images/' + dataRecord.video_image + '"/>';
+	        		return '<img style="margin-left: 5px;" height="90" width="90" src="<?php echo Yii::app()->request->baseUrl; ?>/images/' + dataRecord.video_image + '"/>';
 	        		}
 	        	},
 	        	{text: "View", datafield: "video_total_view", width: 70},
@@ -47,13 +56,64 @@
 
 </script>
 
+<script type="text/javascript">
+	var indexArray =new Array();
+	jQuery(document).ready(function($) {
+		$("#deleteAll").click(function(event) {
+			$.ajax({
+				url: '<?php echo Yii::app()->request->baseUrl; ?>/admin/video/deleteAll',
+				type: 'POST',
+				data: {
+					id: indexArray
+				},
+				success:function(data){
+					$("#jqxgrid").jqxGrid("updatebounddata");
+					$("#jqxgrid").jqxGrid('clearselection');
+					$("#result").html(data);
+                    $("#error").show();
+				},
+			});
+		indexArray.length=0;
+		});
 
+		$('#jqxgrid').on('rowselect', function (event) {
+			indexArray.length=0;
+		    var args = event.args; 
+		    var row = args.rowindex;
+		    if(typeof row.length == 'undefined'){
+		        var dataRecord = $("#jqxgrid").jqxGrid('getrowdata', row);
+		        indexArray.push(dataRecord.video_id);
+		    }else{
+	            for(var i=0 ; i<row.length ; i++){
+	      	        var dataRecord = $("#jqxgrid").jqxGrid('getrowdata', i);
+	      	        indexArray.push(dataRecord.video_id);
+		        }
+		    }
+		});
+		$("#error").hide();
+		$("#txt-search").jqxInput({placeHolder: "Search", height: 25, width: 250, theme:'bootstrap'});	
+	});
+</script>
 <div class="row">
     <div class="col-lg-12">
         <h3 class="alert alert-success">Video Management</h3>
     </div>
 </div>
 
+<div class="row" style="margin-left: 1px;">
+    <p>
+    	<button type="button" class="btn btn-sm btn-primary" id="deleteAll">Delete</button>
+    	<button type="button" class="btn btn-sm btn-primary" id="addNew">Edit</button>
+        <button type="button" class="btn btn-sm btn-primary" id="addNew">Add New</button>
+        <span style="padding-left:50px;">
+            <input type="text" name="txt-search" id="txt-search">
+            <button type="button" class="btn btn-sm btn-primary" id="btn-search" value="Search">Search</button>
+        </span>
+    </p>    
+</div>
+<div class="row" id="error" style="margin-left: 1px;">
+    <div class="alert alert-danger" id="result"></div>
+</div>
 <div class="row" style="margin-left: 1px; ">
     <div id='jqxWidget' style="font-size: 13px; font-family: Verdana; float: left;">
         <div id="jqxgrid"></div>
