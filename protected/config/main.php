d4a97f12708bea97252146922fd5161d8cb6c9c0
anhcuongone <anhcuongone@gmail.com>
2014-04-20 14:29:05 +0700

    ggggg

--- protected/config/main.php
+++ protected/config/main.php
@@ -75,6 +75,8 @@ return array(
 				array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
+					//'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+                	//'ipFilters'=>array('127.0.0.1'),
 				),
 				// uncomment the following to show log messages on web pages
 				/*
